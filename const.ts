145ed55a4736695d46de8d96ff5caa4d2118a1c3
Tarwar <69816954+hermessecund@users.noreply.github.com>
2024-03-16 03:24:06 +0000

    Update const.ts

--- const.ts
+++ const.ts
@@ -1,3 +1,3 @@
 export const activeChain = "mumbai";
-export const factoryAddress = "0x63f22031a7176270A0d53589a73D27662d5A499F";
-export const editionDropAddress = "0x8e100Cce7889A6712687Beb51f72517DA17042ef";
+export const factoryAddress = "0x83eE9EF2CE5072F44FD14bf575CA6bB2D704F734";
+export const editionDropAddress = "0x63f22031a7176270A0d53589a73D27662d5A499F";
